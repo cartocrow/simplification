70f9b4c0b210b9f4d2f15d961918330c90c3fe3f
Meulemans <w.meulemans@tue.nl>
2026-01-30 12:52:51 +0100

    starting on some persistent settings

---
+++ frontend/persistent_settings.cpp
@@ -0,0 +1,106 @@
+#include "persisent_settings.h"
+
+#include <fstream>
+
+PersistentSettings::PersistentSettings(std::string settings) {
+		m_path = settings + ".json";
+		std::ifstream f(m_path);
+		if (!f.fail()) {
+			f >> m_json;
+		}
+		m_changed = false;
+	}
+	
+	PersistentSettings::~PersistentSettings() {
+		if (m_changed) {
+			std::ofstream f(m_path);
+			f << std::setw(4) << m_json << std::endl;
+		}
+	}
+	
+	void PersistentSettings::setColor(Key key, Color value) {
+		m_json[key] = { value.red(), value.green(), value.blue() };
+		m_changed = true;
+	}
+	
+	Color PersistentSettings::getColor(Key key, Color default_value) {
+		Json& obj = m_json[key];
+		if (obj.is_null()) {
+			return default_value;
+		} else {
+			std::vector<int> rgb = obj.get<std::string>();
+			return Color{rgb[0], rgb[1], rgb[2] };
+		}	
+		
+	}
+	
+	void PersistentSettings::setPath(Key key, std::filesystem::path value) {
+		m_json[key] = value.string();
+		m_changed = true;
+	}
+	
+	std::filesystem::path PersistentSettings::getPath(Key key, std::filesystem::path default_value) {
+		Json& obj = m_json[key];
+		if (obj.is_null()) {
+			return default_value;
+		} else {
+			return obj.get<std::string>();
+		}	
+	}
+	
+	void PersistentSettings::setString(Key key, std::string value) {
+		m_json[key] = value;
+		m_changed = true;
+	}
+	
+	std::string PersistentSettings::getString(Key key, std::string default_value) {
+		Json& obj = m_json[key];
+		if (obj.is_null()) {
+			return default_value;
+		} else {
+			return obj.get<std::string>();
+		}	
+	}
+			
+	void PersistentSettings::setBoolean(Key key, bool value) {
+		m_json[key] = value;
+		m_changed = true;
+	}
+	
+	bool PersistentSettings::getBoolean(Key key, bool default_value) {
+		Json& obj = m_json[key];
+		if (obj.is_null()) {
+			return default_value;
+		} else {
+			return obj.get<bool>();
+		}	
+	}
+	
+	void PersistentSettings::setInteger(Key key, int value) {
+		m_json[key] = value;
+		m_changed = true;
+	}
+	
+	int PersistentSettings::getInteger(Key key, int default_value) {
+		Json& obj = m_json[key];
+		if (obj.is_null()) {
+			return default_value;
+		} else {
+			return obj.get<int>();
+		}	
+	}
+			
+	void PersistentSettings::setDouble(Key key, double value) {
+		m_json[key] = value;
+		m_changed = true;
+	}
+	
+	int PersistentSettings::getDouble(Key key, double default_value) {
+		Json& obj = m_json[key];
+		if (obj.is_null()) {
+			return default_value;
+		} else {
+			return obj.get<double>();
+		}	
+	}
+	
